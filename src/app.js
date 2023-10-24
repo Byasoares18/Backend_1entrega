d9d226a4a936a9967116a7efffbc8099a5996032
Byasoares18 <105671476+Byasoares18@users.noreply.github.com>
2023-10-23 23:41:10 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import routerProducts from './router/products.router.js'
+import routerCarts from './router/carts.router.js'
+
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('./src/public'))
+
+app.get('/', (req, res) => res.json('Mi tienda'));
+app.use('/api/products', routerProducts)
+app.use('/api/carts', routerCarts)
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
