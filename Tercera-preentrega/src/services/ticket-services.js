274cbdcbbe925a35f11f0434205270b6bda2cc15
Byasoares18 <105671476+Byasoares18@users.noreply.github.com>
2024-09-13 22:01:23 -0300

    final

--- Tercera-preentrega/src/services/ticket-services.js
+++
@@ -1,8 +0,0 @@
-export default class TicketServices {
-
-    constructor(dao){
-        this.dao= dao
-    }
-    
-    createTicket = async (cid, user) => {return this.dao.createTicket(cid, user)}
-}
